8f16e1837034d76d7fa7b93e709b6306913e7d50
yasuaki.osada <dice.worker@gmail.com>
2019-08-23 22:03:12 +0900

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Yasuaki Osada on 2019/08/23.
+//  Copyright © 2019 Yasuaki Osada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
